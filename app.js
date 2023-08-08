1b936a8652b725485cba94e34d613804cac0a36a
kathecorner <40414604+kathecorner@users.noreply.github.com>
2023-08-08 13:20:42 +0900

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/api/v1/tasks", taskRoute);
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URL);
-            app.listen(PORT, console.log("the server is running at app.js."));
+            app.listen(process.env.PORT || PORT, console.log("the server is running at app.js."));
 
     } catch(err) {
         console.log(err);
